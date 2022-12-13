1a8e5a1a529e1ea87a73af3f035a9f3849edd898
SebastienZhang <zhangsebastien1902@gmail.com>
2022-12-13 13:12:34 +0900

    Removing non finished script

--- Assets/UI/Runtime/EntryData.cs
+++ Assets/UI/Runtime/EntryData.cs
@@ -5,18 +5,5 @@ using UnityEngine.UI;
 
 public class EntryData : MonoBehaviour
 {
-    MovementsInput movementsInput;
-    public TextMesh movementsList;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        movementsList.text = ""
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
